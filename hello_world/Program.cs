0a3fcbcf4bf157ec5ae701e66870ee8199a07134
David Colatti <davidcolatti@gmail.com>
2020-11-18 15:46:17 -0500

    feat: learning c#

---
+++ hello_world/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for loop
+            //for(int counter = 0; counter < 10; counter++)
+            //{
+            //    Console.WriteLine(counter);
+            //}
+
+
+            //foreach loop
+            //int[] arrInts = new int[] { 3, 5, 6, 23, 95, 45, 32 };
+
+            //foreach (int item in arrInts)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //while loop
+            //int sentinel = 0;
+            //while(sentinel < 10)
+            //{
+            //    Console.WriteLine(sentinel);
+            //    sentinel++;
+            //}
+
+            //do while loop
+            //int sentinel = 0;
+            //do
+            //{
+            //    Console.WriteLine(sentinel);
+            //    sentinel++;
+            //} while (sentinel < 10);
+
+            // Decision Structures
+
+            //if statement
+            //bool result = true;
+            //if(result)
+            //{
+            //    Console.WriteLine("Result was true");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Result was false");
+            //}
+
+            //if..else if
+            int value = 0;
+            if(value == 0)
+            {
+                Console.WriteLine("Value is 0");
+            }
+            else if (value == 1)
+            {
+                Console.WriteLine("Value is not 0");
+            }
+            else
+            {
+                Console.WriteLine("Value is something else");
+            }
+        }
+    }
+}
